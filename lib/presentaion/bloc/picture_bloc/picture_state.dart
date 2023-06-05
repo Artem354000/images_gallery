9480483f2b623b3b786217af61c8c869df312356
Artem354000 <artem.luskan@gmail.com>
2023-06-05 18:28:37 +0300

    Addition v.2

--- lib/presentaion/bloc/picture_bloc/picture_state.dart
+++ lib/presentaion/bloc/picture_bloc/picture_state.dart
@@ -1,7 +1,7 @@
 part of 'picture_bloc.dart';
 
 @freezed
-class PictureState with _$PictureState {
+sealed class PictureState with _$PictureState {
   const factory PictureState.loading() = _PictureLoadingState;
   const factory PictureState.loaded({required List<Picture> picturesLoaded}) =
       _PictureLoadedState;
--- lib/presentaion/bloc/picture_of_item_cubit/picture_of_item_state.dart
+++ lib/presentaion/bloc/picture_of_item_cubit/picture_of_item_state.dart
@@ -1,7 +1,7 @@
 part of 'picture_of_item_cubit.dart';
 
 @freezed
-class PictureOfItemState with _$PictureOfItemState {
+sealed class PictureOfItemState with _$PictureOfItemState {
   const factory PictureOfItemState.pictureOfItem({required Picture? picture}) =
       _PictureOfItemState;
 }
